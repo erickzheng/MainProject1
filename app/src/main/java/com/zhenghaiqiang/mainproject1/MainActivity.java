90ba606f7add67e77536a0043031f9de435582d3
zhenghaiqiang <it_zhq@qq.com>
2016-09-27 17:06:14 +0800

    conflickt

--- app/src/main/java/com/zhenghaiqiang/mainproject1/MainActivity.java
+++ app/src/main/java/com/zhenghaiqiang/mainproject1/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Util1.toast(this,"sdfkjsdklf");
+		Util1.toast(this,"sdfkjsdklf");
     }
 }
